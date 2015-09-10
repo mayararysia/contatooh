18c776a59c3287ad6b9dc0323d0cf02cdb8d675b
mayararysia <mayara.ryzia@gmail.com>
2015-09-10 15:56:19 -0300

    removendo comentários

--- config/protractor.js
+++ config/protractor.js
@@ -1,24 +1,5 @@
 //config/protractor.js
 
-//exports.config = {
- 
-  //specs: ['../test/e2e/**/*.js'],
- /* onPrepare: function() {
-    browser.driver.get('http://localhost:3000');
-
-    browser.wait(function(){
-      return browser.driver.isElementPresent(by.id('entrar'));
-    });
-
-    browser.driver.findElement(by.id('entrar')).click();
-   
-    browser.driver.findElement(by.id('login_field'))
-      .sendKeys('mayara_rysia@hotmail.com');
-    browser.driver.findElement(by.id('password'))
-        .sendKeys('mayteste123');
-    browser.driver.findElement(by.name('commit')).click();
-  }*/
-
   var config = require('./config')();
 
   browser.driver.findElement(by.id('login_field'))
