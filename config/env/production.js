037e3acc735d24d419c7eb56b998b4db7e6d3c06
mayararysia <mayara.ryzia@gmail.com>
2015-09-15 10:05:49 -0300

    ID substituído por IP

--- config/env/production.js
+++ config/env/production.js
@@ -4,6 +4,6 @@ module.exports = {
 	clientID: process.env.CLIENT_ID,
 	clientSecret: process.env,CLIENT_SECRET,
 	port: process.env.OPENSHIFT_NODEJS_PORT,
-	address: process.env.OPENSHIFT_NODEJS_ID,
+	address: process.env.OPENSHIFT_NODEJS_IP,
 	domain: process.env.OPENSHIFT_APP_DNS
 };
